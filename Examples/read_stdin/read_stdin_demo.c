24d6a00440d24d168502bb786dd1b8e72eb32dd7
Trenix25 <trenix25@pm.me>
2019-06-18 16:12:27 -0700

    Uploaded Examples

---
+++ Examples/read_stdin/read_stdin_demo.c
@@ -0,0 +1,42 @@
+/*
+
+     read_stdin_demo.c
+     This file offers a simple example of how to use read_stdin().
+     Written by Matthew Campbell.
+
+*/
+
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+/* Prototype read_stdin() */
+
+int read_stdin( char *buffer, const int length, const char *prompt );
+
+int main( void )
+{
+     char buffer[ 1024 ];
+     int ret, save_errno;
+
+     errno = 0;
+     ret = read_stdin( buffer, 1024, "prompt >> " );
+     save_errno = errno;
+     if ( ret != 0 )
+     {
+          printf( "Something went wrong." );
+          if ( save_errno != 0 )
+          {
+               printf( "  Error: %s.\n", strerror( save_errno ) );
+          }
+          else
+          {
+               printf( "\n" );
+          }
+          return 1;
+     }
+     printf( "Received: \"%s\"\n", buffer );
+     return 0;
+}
+
+/* EOF read_stdin_demo.c */
